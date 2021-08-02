1937c6763c1b80109a0e3f4f9e0acb47f85557a6
moonzpwr <60944420+moonzpwr@users.noreply.github.com>
2021-08-02 17:04:21 +0300

    without RxJs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 
+import { useEffect, useState } from 'react';
+// import { interval } from 'rxjs';
+// import {take, map, scan} from 'rxjs/operators'
 import './App.css';
 
+
 function App() {
+  const [string, setString] = useState('')
+  const [color, setColor] = useState('black')
+
+  
+  // first render str
+   useEffect(() => {
+    let str = [];
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for (let i = 0; i < 5; i += 1) {
+      str.push(possible.charAt(Math.floor(Math.random() * possible.length)))
+      }
+    
+      setString(str.join(''))
+   }, [])
+  
+  //every 3sec render
+  useEffect(() => {
+    let symbols = [];
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    const interval = setInterval(() => {
+      for (let i = 0; i < 5; i += 1) {
+      symbols.push(possible.charAt(Math.floor(Math.random() * possible.length)))
+      }
+
+      const str = symbols.join('')
+      setString(str)
+
+      setColor('black')
+     
+      if (str.toLowerCase() === str.toLowerCase().split('').reverse().join('')) {
+      setColor('red')
+      } else if (parseInt(str).toString().length === 5) {
+      setColor('blue')
+      }
+      
+    }, 3000)
+    
+    return () => {
+      clearInterval(interval)
+    }
+  }, [string])
+
   return (
-    <div >
-      hello world
+    <div style={{color: color}} className='container'>
+      {!string.includes(0) && string}
     </div>
   );
 }
